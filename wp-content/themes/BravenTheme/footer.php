6db603504968c90ca3aa74c1c6a8480a139e4241
Staging Server <tech@bebraven.org>
2016-01-06 13:59:34 -0500

    Move some inline javascript from header to footer for better PageSpeed ranking.

--- wp-content/themes/BravenTheme/footer.php
+++ wp-content/themes/BravenTheme/footer.php
@@ -28,5 +28,62 @@
 </div>
 <!-- #page -->
 
+<?php if (is_home() || is_front_page() ) : ?> 
+	<script src="<?php bloginfo('template_directory')?>/js/jquery.bxslider.js"></script>
+	<script>
+		jQuery(document).ready(function($){
+			$('#slider_posts').bxSlider({
+				mode: 'horizontal',
+				useCSS: false,
+				infiniteLoop: true,
+				hideControlOnEnd: true,
+				easing: 'swing',
+				auto:true,
+				pause: 8000,
+				speed: 2000
+			});
+			$('#slider_testimonials').bxSlider({
+				mode: 'horizontal',
+				useCSS: false,
+				infiniteLoop: true,
+				hideControlOnEnd: true,
+				easing: 'swing',
+				mode: 'fade',
+				auto:true,
+				pause: 7000,
+				speed: 2800
+			});
+		}); // end JQuery
+		/*$('a').click(function(){
+			$('html, body').animate({
+				scrollTop: $('[name="' + $.attr(this, 'href').substr(1) + '"]').offset().top
+			}, 500);
+			return false;
+		});*/
+	</script>
+<?php endif; // is home || is front page ?>
+<?php if (is_page_template( 'page-with-charts.php' ) || is_home() || is_front_page() ) : ?>
+	<script src="<?php echo bloginfo('template_directory')?>/js/jquery.easypiechart.min.js"></script>
+	<script src="<?php echo bloginfo('template_directory')?>/js/wow.min.js"></script>
+	<script>
+		jQuery(document).ready(function($){
+			$('.chart').easyPieChart({
+				easing: 'easeOutBounce',
+				animate: 6000,
+				delay:8000,
+				onStep: function(from, to, percent) {
+					$(this.el).find('.percent').text(Math.round(percent));
+				}
+			});
+			var chart = window.chart = $('.chart').data('easyPieChart');
+			$('.js_update').on('click', function() {
+				chart.update(Math.random()*200-100);
+			});
+		}); // end JQuery
+		wow = new WOW();
+		wow.init();
+	</script>
+<?php endif; ?>
+
 <?php wp_footer(); ?>
 </body></html>
